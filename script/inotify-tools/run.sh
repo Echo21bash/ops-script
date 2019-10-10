2018c21eedb677d43e24abda5e6e9de2a8ca01b5
hebaodanroot <45256479+hebaodanroot@users.noreply.github.com>
2019-10-10 20:48:50 +0800

    Update run.sh

--- script/inotify-tools/run.sh
+++ script/inotify-tools/run.sh
@@ -16,7 +16,7 @@ creat_all_file_name(){
 	for i in ${file_dir[@]};
 	do
 		if [ -d $i ];then
-			find ${i}/* -maxdepth 0 -name "*${file_type}" >> ${file_list}
+			find ${i} -maxdepth 1 -name "*${file_type}" >> ${file_list}
 		fi
 	done
 }
