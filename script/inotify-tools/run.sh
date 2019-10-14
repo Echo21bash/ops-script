02da7f983ed04881362183b4b999b72f3a940ca3
hebaodanroot <45256479+hebaodanroot@users.noreply.github.com>
2019-10-14 14:27:29 +0800

    Update run.sh

--- script/inotify-tools/run.sh
+++ script/inotify-tools/run.sh
@@ -23,30 +23,33 @@ creat_all_file_name(){
 
 listen_file(){
 
-	inotifywatch -v -t ${time_out} -e access  --fromfile ${file_list} | awk '{print $2"\t"$3}' > /tmp/report.txt
+	while :
+	do
+		inotifywatch -v -t ${time_out} -e access  --fromfile ${file_list} | awk '{print $2"\t"$3}' > /tmp/report.txt
+		if [ $? = 0 ];then
+			break
+		fi
+	done
 }
 
 add_report(){
-	report=$(cat /tmp/report.txt | wc -l)
-	if [[ ${report} > 0 ]];then
-		while :
-		do
-			awk 'NR==FNR{a[$2]=$0;print}NR>FNR{if($1 in a);else print 0"\t"$0}' /tmp/report.txt ${file_list} >${read_report}
-			if [ $? = 0 ];then
-				break
-			fi
-		done
+	report=$(cat /tmp/report.txt | grep -E -o ^total)
+	if [[ -n ${report} ]];then
+		awk 'NR==FNR{a[$2]=$0;print}NR>FNR{if($1 in a);else print 0"\t"$0}' /tmp/report.txt ${file_list} >${read_report}
+		
 	fi
 }
 
 delete_file(){
-	total_num=`awk 'END{print NR}' $read_report`
-	save_num=`echo ${total_num}-${total_num}*${ratio}|bc`
-	#生成删除文件列表
-	awk 'NR>'${save_num}'{print $2}' ${read_report} >${delete_file}
-	awk '{if ($1<'${access}')print $2}' ${read_report} >>${delete_file}
-	#删除文件
-	awk '{print}' ${delete_file} | xargs -n 1 rm -rf
+	if [ -s ${read_report} ];then
+		total_num=`awk 'END{print NR}' $read_report`
+		save_num=`echo ${total_num}-${total_num}*${ratio}|bc`
+		#生成删除文件列表
+		awk 'NR>'${save_num}'{print $2}' ${read_report} >${delete_file}
+		awk '{if ($1<'${access}')print $2}' ${read_report} >>${delete_file}
+		#删除文件
+		awk '{print}' ${delete_file} | xargs -n 1 rm -rf
+	fi
 }
 
 mkdir -p ${logs_dir}
