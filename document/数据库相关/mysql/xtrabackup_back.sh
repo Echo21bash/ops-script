4a21766e536c8305aab30ba7814bfcc0a99ed6a3
hebaodan <hebaodan21@qq.com>
2020-09-25 16:20:37 +0800

    updata

--- document/数据库相关/mysql/xtrabackup_back.sh
+++ document/数据库相关/mysql/xtrabackup_back.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+mysql_user='root'
+mysql_passwd='123456'
+mysql_config_file='/opt/mysql/my.cnf'
+remote_host='10.255.50.144'
 
 date=`date +%Y%m%d`
 echo `date +%Y%m%d-%H%M`：开始备份 >> backup_db.log
 
 echo "------ start backup db ------"
 
-ssh root@10.255.50.144 \ "mkdir -p /data/backup/database/`date +%Y%m%d`"
+ssh ${remote_host} \ "mkdir -p /data/backup/database/`date +%Y%m%d`"
 
 echo `date +%Y%m%d-%H%M`：创建目录-$date >> backup_db.log
 
-innobackupex --defaults-file=/opt/mysql/my.cnf --no-lock --user 'root' --password '123456' --stream=tar ./ | ssh root@10.255.50.144 \ "cat - > /data/backup/database/`date +%Y%m%d`/`date +%H-%M`-backup.tar"
+innobackupex --defaults-file=${mysql_config_file} --no-lock --user=${mysql_user} --password=${mysql_passwd} --stream=tar ./ | gzip | ssh ${remote_host} \ "cat - > /data/backup/database/`date +%Y%m%d`/`date +%H-%M`-backup.tar"
 
 echo `date +%Y%m%d-%H%M`：备份结束 >> backup_db.log
 
