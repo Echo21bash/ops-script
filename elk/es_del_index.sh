fecada049f2239e3f7fca51c2eb612af421bc775
hebaodan <hebaodan21@qq.com>
2020-11-02 12:58:32 +0800

    updata

--- elk/es_del_index.sh
+++ elk/es_del_index.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
 
 ###################################
-#删除早于十天的ES集群的索引
+#删除ES集群的索引
 ###################################
 es_ip='172.26.32.173:9200'
+es_user='elastic'
+es_passwd='123456'
+#索引保留天数
 day=10
-while true
-do
-	((day++))
-	comp_date=`date -d "${day} day ago" +"%Y.%m.%d"`
-	if [[ ! -z `curl -XGET http://${es_ip}/_cat/indices | awk '{print $3}' | grep "${comp_date}"` ]];then
-		curl -XGET http://${es_ip}/_cat/indices | awk '{print $3}' | grep "${comp_date}" | while read LINE
-		do
-			curl -XDELETE http://${es_ip}/${LINE}
-			if [[ $? = '0' ]];then
-			echo "${LINE}删除成功"
-			fi
-		done
-	else
-		break
-	fi
-done
+no_passwd(){
+	while true
+	do
+		((day++))
+		comp_date=`date -d "${day} day ago" +"%Y.%m.%d"`
+		if [[ ! -z `curl -k -XGET http://${es_ip}/_cat/indices | awk '{print $3}' | grep "${comp_date}"` ]];then
+			curl -XGET http://${es_ip}/_cat/indices | awk '{print $3}' | grep "${comp_date}" | while read LINE
+			do
+				curl -k -XDELETE http://${es_ip}/${LINE}
+				if [[ $? = '0' ]];then
+				echo "${LINE}删除成功"
+				fi
+			done
+		else
+			break
+		fi
+	done
+}
 
+passwd(){
+	while true
+	do
+		((day++))
+		comp_date=`date -d "${day} day ago" +"%Y.%m.%d"`
+		if [[ ! -z `curl -k -u ${es_user}:${es_passwd} -XGET http://${es_ip}/_cat/indices | awk '{print $3}' | grep "${comp_date}"` ]];then
+			curl -k -u ${es_user}:${es_passwd} -XGET http://${es_ip}/_cat/indices | awk '{print $3}' | grep "${comp_date}" | while read LINE
+			do
+				curl -k -u ${es_user}:${es_passwd} -XDELETE http://${es_ip}/${LINE}
+				if [[ $? = '0' ]];then
+				echo "${LINE}删除成功"
+				fi
+			done
+		else
+			break
+		fi
+	done
+}
+passwd
