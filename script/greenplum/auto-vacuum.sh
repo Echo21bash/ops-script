8c7811de1bde800b33c4e5bcdb4e3509040b455a
hebaodan <hebaodan21@qq.com>
2020-12-29 14:51:48 +0800

    updata

--- script/greenplum/auto-vacuum.sh
+++ script/greenplum/auto-vacuum.sh
@@ -7,8 +7,10 @@ log_file='/home/gpadmin/itp_tool/vacuum.log'
 tables_file='/home/gpadmin/itp_tool/tables.txt'
 dblist=`psql -d postgres -c "copy (select datname from pg_stat_database) to stdout"`
 dblist=($dblist)
-dblist=('tyacc_tytest')
-schema_name=('AFC_ITP_BUSINESS' 'AFC_ITP_SDK' )
+dblist=('tyacc_tytest' 'lcdb')
+#申明关联数组
+declare -A schema_name
+schema_name=([tyacc_tytest]='AFC_ITP_BUSINESS AFC_ITP_SDK' [lcdb]='afc_business')
 start_time=`date`
 
 run_vacuum(){
@@ -17,14 +19,15 @@ if [[ -z $exec ]];then
 fi
 for db in ${dblist[@]} ; do
     #skip system databases
-    if [[ $db == template0 ]] ||  [[ $db == template1 ]] || [[ $db == postgres ]] || [[ $db =        = gpdb ]] ; then
+    if [[ $db == template0 ]] ||  [[ $db == template1 ]] || [[ $db == postgres ]] || [[ $db == gpdb ]] ; then
         continue
     fi
         echo processing db "$db"
         #use a temp file to store the table list, which could be vary large
         >$tables_file
         #query out only the normal user tables, excluding partitions of parent tables
-        for schema in ${schema_name[@]}
+        schema_list=(${schema_name[$db]})
+        for schema in ${schema_list[@]}
         do
                 psql -d $db -c "copy (select '\"'||tables.schemaname||'\".' || '\"'||tables.tablename||'\"' from (select nspname as schemaname, relname as tablename from pg_catalog.pg_class, pg_catalog.pg_namespace, pg_catalog.pg_roles where pg_class.relnamespace = pg_namespace.oid and pg_namespace.nspowner = pg_roles.oid and pg_class.relkind='r' and (pg_namespace.nspname = '${schema}' or pg_roles.rolsuper = 'false' ) ) as tables(schemaname, tablename) left join pg_catalog.pg_partitions on pg_partitions.partitionschemaname=tables.schemaname and pg_partitions.partitiontablename=tables.tablename where pg_partitions.partitiontablename is null) to stdout;" >>$tables_file
         done
