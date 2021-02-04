0357f5be85381a4463c99e04bbe14726e85f21bb
hebaodan <hebaodan21@qq.com>
2021-02-04 18:24:43 +0800

    updata

--- zabbix/ocfs2/ocfs2_status.sh
+++ zabbix/ocfs2/ocfs2_status.sh
@@ -8,17 +8,17 @@ case "$1" in
 	cluster_status)
 		status=`o2cb.init status | grep cluster | grep -o Online`
 		if [[ $status = 'Online' ]];then
-			exit 0
+			echo 0
 		else
-			exit 1
+			echo 1
 		fi
 		;;
 	heartbeat_status)
 		status=`o2cb.init status | grep heartbeat | grep -o Active`
 		if [[ $status = 'Active' ]];then
-			exit 0
+			echo 0
 		else
-			exit 1
+			echo 1
 		fi
 		;;
 	*)
