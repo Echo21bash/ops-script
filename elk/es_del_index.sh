2f3d9df56e87ac25f17a7e53948c5367a2af5af7
hebaodan <hebaodan21@qq.com>
2021-02-23 16:52:43 +0800

    修改删除ES索引方法

--- elk/es_del_index.sh
+++ elk/es_del_index.sh
@@ -4,45 +4,44 @@
 #删除ES集群的索引
 ###################################
 es_ip='172.26.32.173:9200'
-es_user='elastic'
-es_passwd='123456'
+#es_user='elastic'
+#es_passwd='123456'
 #索引保留天数
 day=10
-no_passwd(){
+
+get_index(){
+	if [[ -z ${es_user} ]];then
+		curl -k -XGET http://${es_ip}/_cat/indices | awk '{print $3}' >index.txt
+	else
+		curl -k -u ${es_user}:${es_passwd} -XGET http://${es_ip}/_cat/indices | awk '{print $3}' >index.txt
+	fi
+
+}
+
+delete_index(){
 	while true
 	do
 		((day++))
 		comp_date=`date -d "${day} day ago" +"%Y.%m.%d"`
-		if [[ ! -z `curl -k -XGET http://${es_ip}/_cat/indices | awk '{print $3}' | grep "${comp_date}"` ]];then
-			curl -XGET http://${es_ip}/_cat/indices | awk '{print $3}' | grep "${comp_date}" | while read LINE
+		if [[ ! -z `cat index.txt | grep "${comp_date}" | grep "${comp_date}"` ]];then
+			cat index.txt | grep "${comp_date}" | while read LINE
 			do
 				curl -k -XDELETE http://${es_ip}/${LINE}
 				if [[ $? = '0' ]];then
-				echo "${LINE}删除成功"
+					echo "${LINE}删除成功"
 				fi
 			done
 		else
 			break
 		fi
+		
 	done
+
 }
 
-passwd(){
-	while true
-	do
-		((day++))
-		comp_date=`date -d "${day} day ago" +"%Y.%m.%d"`
-		if [[ ! -z `curl -k -u ${es_user}:${es_passwd} -XGET http://${es_ip}/_cat/indices | awk '{print $3}' | grep "${comp_date}"` ]];then
-			curl -k -u ${es_user}:${es_passwd} -XGET http://${es_ip}/_cat/indices | awk '{print $3}' | grep "${comp_date}" | while read LINE
-			do
-				curl -k -u ${es_user}:${es_passwd} -XDELETE http://${es_ip}/${LINE}
-				if [[ $? = '0' ]];then
-				echo "${LINE}删除成功"
-				fi
-			done
-		else
-			break
-		fi
-	done
+main(){
+	get_index
+	delete_index
 }
-passwd
+
+main
