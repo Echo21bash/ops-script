fbe413c76173b70b8a24674323901180c1e52cd1
hebaodan <hebaodan21@qq.com>
2021-02-24 10:25:38 +0800

    优化删除ES索引逻辑

--- elk/es_del_index.sh
+++ elk/es_del_index.sh
@@ -2,46 +2,56 @@
 
 ###################################
 #删除ES集群的索引
+#适用于索引按天创建并且日期格式为2020.01.01
 ###################################
+log_file='del_index.log'
+index_file='index.txt'
 es_ip='172.26.32.173:9200'
 #es_user='elastic'
 #es_passwd='123456'
 #索引保留天数
-day=10
+day=30
+
+public(){
+	if [[ -z ${es_user} ]];then
+		exec="curl -k -s"
+	else
+		exec="curl -k -s -u ${es_user}:${es_passwd}"
+	fi
+
+}
 
 get_index(){
 	if [[ -z ${es_user} ]];then
-		curl -k -XGET http://${es_ip}/_cat/indices | awk '{print $3}' >index.txt
+		${exec} -XGET http://${es_ip}/_cat/indices | awk '{print $3}' >${index_file}
 	else
-		curl -k -u ${es_user}:${es_passwd} -XGET http://${es_ip}/_cat/indices | awk '{print $3}' >index.txt
+		${exec} -XGET http://${es_ip}/_cat/indices | awk '{print $3}' >${index_file}
 	fi
 
 }
 
 delete_index(){
-	while true
+	comp_date=`date -d "${day} day ago" +"%Y.%m.%d"`
+	cat index.txt | grep -oE [0-9]{4}\.[0-9]{2}\.[0-9]{2} | sort -u | while read data
 	do
-		((day++))
-		comp_date=`date -d "${day} day ago" +"%Y.%m.%d"`
-		if [[ ! -z `cat index.txt | grep "${comp_date}" | grep "${comp_date}"` ]];then
-			cat index.txt | grep "${comp_date}" | while read LINE
+		if [[ ${data} < ${comp_date} ]];then
+			cat index.txt | grep ${data} | while read LINE
 			do
-				curl -k -XDELETE http://${es_ip}/${LINE}
-				if [[ $? = '0' ]];then
+				res=`${exec} -IL -XDELETE http://${es_ip}/${LINE} | grep 'HTTP/1.1 200'`
+				if [[ -n ${res} ]];then
 					echo "${LINE}删除成功"
+				else
+					echo "${LINE}删除失败"
 				fi
 			done
-		else
-			break
 		fi
-		
 	done
-
 }
 
 main(){
-	get_index
-	delete_index
+	public >>${log_file}
+	get_index >>${log_file}
+	delete_index >>${log_file}
 }
 
 main
