5d02e46cc659ab0ac726146dd9034f56707295ae
hebaodan <hebaodan21@qq.com>
2021-03-12 18:34:35 +0800

    优化Vacuum脚本

--- script/greenplum/auto-vacuum.sh
+++ script/greenplum/auto-vacuum.sh
@@ -3,52 +3,90 @@
 #do not echo command, just get a list of db
 source /usr/local/greenplum-db/greenplum_path.sh
 exec=$1
-log_file='/home/gpadmin/itp_tool/vacuum.log'
-tables_file='/home/gpadmin/itp_tool/tables.txt'
+work_dir='/home/gpadmin/itp_tool'
+log_file="${work_dir}/vacuum.log"
 dblist=`psql -d postgres -c "copy (select datname from pg_stat_database) to stdout"`
 dblist=($dblist)
-dblist=('tyacc_tytest' 'lcdb')
+dblist=('lcdb' 'tyacc_production')
 #申明关联数组
 declare -A schema_name
-schema_name=([tyacc_tytest]='AFC_ITP_BUSINESS AFC_ITP_SDK' [lcdb]='afc_business')
-start_time=`date +'%Y-%m-%d %H:%M:%S'`
+schema_name=([tyacc_production]='AFC_ITP_BUSINESS AFC_ITP_SDK AFC_ITP_BIZ_COL AFC_ITP_FACE' [lcdb]='afc_txn')
 
-run_vacuum(){
-if [[ -z $exec ]];then
-    exec='VACUUM'
-fi
-for db in ${dblist[@]} ; do
-    #skip system databases
-    if [[ $db == template0 ]] ||  [[ $db == template1 ]] || [[ $db == postgres ]] || [[ $db == gpdb ]] ; then
-        continue
-    fi
-        echo " 正在处理$db"
-        #use a temp file to store the table list, which could be vary large
-        >$tables_file
-        #query out only the normal user tables, excluding partitions of parent tables
-        schema_list=(${schema_name[$db]})
-        for schema in ${schema_list[@]}
-        do
-                psql -d $db -c "copy (select '\"'||tables.schemaname||'\".' || '\"'||tables.tablename||'\"' from (select nspname as schemaname, relname as tablename from pg_catalog.pg_class, pg_catalog.pg_namespace, pg_catalog.pg_roles where pg_class.relnamespace = pg_namespace.oid and pg_namespace.nspowner = pg_roles.oid and pg_class.relkind='r' and (pg_namespace.nspname = '${schema}' or pg_roles.rolsuper = 'false' ) ) as tables(schemaname, tablename) left join pg_catalog.pg_partitions on pg_partitions.partitionschemaname=tables.schemaname and pg_partitions.partitiontablename=tables.tablename where pg_partitions.partitiontablename is null) to stdout;" >>$tables_file
-        done
-        while read line; do
-                #some table name may contain the $ sign, so escape it
-                line=`echo $line |sed 's/\\\$/\\\\\\\$/g'`
-                #vacuum full this table, which will lock the table
-				psql -d $db -e -a -c "$exec $line;" && psql -d $db -e -a -c "set lock_timeout = '3s';REINDEX TABLE $line;" &
-        done <$tables_file
-        #reindex system tables firstly
-        psql -d $db -e -a -c "REINDEX SYSTEM $db;"
-        #更新统计数据
-        psql -d $db -e -a -c "ANALYZE VERBOSE;"
 
-done
+get_all_tables(){
+
+	tables_file="${work_dir}/${db}_table_list.txt"
+	>${tables_file}
+	#skip system databases
+	if [[ $db == template0 ]] || [[ $db == template1 ]] || [[ $db == postgres ]] || [[ $db == gpdb ]] ; then
+		continue
+	fi
+		
+	#query out only the normal user tables, excluding partitions of parent tables
+	schema_list=(${schema_name[$db]})
+	for schema in ${schema_list[@]}
+	do
+		psql -d $db -c "copy (select '\"'||tables.schemaname||'\".' || '\"'||tables.tablename||'\"' from (select nspname as schemaname, relname as tablename from pg_catalog.pg_class, pg_catalog.pg_namespace, pg_catalog.pg_roles where pg_class.relnamespace = pg_namespace.oid and pg_namespace.nspowner = pg_roles.oid and pg_class.relkind='r' and (pg_namespace.nspname = '${schema}' or pg_roles.rolsuper = 'false' ) ) as tables(schemaname, tablename) left join pg_catalog.pg_partitions on pg_partitions.partitionschemaname=tables.schemaname and pg_partitions.partitiontablename=tables.tablename where pg_partitions.partitiontablename is null) to stdout;" >>${tables_file}
+	done
+
+
 }
 
-echo ${start_time} >>${log_file}
-run_vacuum >>${log_file}
-end_time=`date +'%Y-%m-%d %H:%M:%S'`
-echo ${end_time} >>${log_file}
-start_seconds=$(date --date="$starttime" +%s);
-end_seconds=$(date --date="$endtime" +%s);
-echo "本次运行时间："$((end_seconds-start_seconds))"s" >>${log_file}
+run_vacuum(){
+	
+	
+	if [[ -z $exec ]];then
+		exec='VACUUM'
+	fi
+
+		
+	tables_file="${work_dir}/${db}_table_list.txt"
+	while read line; do
+		#some table name may contain the $ sign, so escape it
+		line=`echo $line |sed 's/\\\$/\\\\\\\$/g'`
+		#vacuum full this table, which will lock the table
+		psql -d $db -L ${log_file} -e -a -c "$exec $line;" && \
+		psql -d $db -L ${log_file} -e -a -c "set lock_timeout = '3s';REINDEX TABLE $line;" && \
+		psql -d $db -L ${log_file} -e -a -c "ANALYZE $line;" &
+	done <${tables_file}
+		
+
+	while true; do
+		sleep 10
+		proc=`psql -e -a -c "select * from pg_stat_activity;" | grep -oiE 'VACUUM|REINDEX|ANALYZE'`
+		if [[ -z $proc ]];then
+			return
+		fi
+	done
+	
+}
+
+run_ctrl(){
+
+	
+	for db in ${dblist[@]} ; do
+		get_all_tables >>${log_file}
+		start_time=`date +'%Y-%m-%d %H:%M:%S'`
+		start_seconds=$(date --date="$start_time" +%s);
+		run_vacuum >>${log_file}
+		end_time=`date +'%Y-%m-%d %H:%M:%S'`
+		end_seconds=$(date --date="$end_time" +%s);
+		echo "本次VACUUM ${db} 运行时间："$((end_seconds-start_seconds))"s" >>${log_file}
+	done
+	
+}
+
+main(){
+
+	start_run_time=`date +'%Y-%m-%d %H:%M:%S'`
+	echo ${start_run_time} >>${log_file}
+	run_ctrl
+	end_run_time=`date +'%Y-%m-%d %H:%M:%S'`
+	echo ${end_run_time} >>${log_file}
+	start_seconds=$(date --date="$start_run_time" +%s);
+	end_seconds=$(date --date="$end_run_time" +%s);
+	echo "本次总运行时间："$((end_seconds-start_seconds))"s" >>${log_file}
+
+}
+
+main
