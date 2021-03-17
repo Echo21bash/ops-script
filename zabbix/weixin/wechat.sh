da78db7f6e6df51b1e1a36dbde3c846c6c9c27dc
github <wwroot21@gmail.com>
2021-03-17 23:49:13 +0800

    优化企业微信脚本

--- zabbix/weixin/wechat.sh
+++ zabbix/weixin/wechat.sh
@@ -28,3 +28,48 @@ printf '\t"safe":"0"\n'
 printf '}\n'
 }
 /usr/bin/curl --data-ascii "$(body)" $PURL
+
+
+load_config_file(){
+
+	if [[ -f $optarg ]];then
+		. $optarg
+	else
+		echo "配置文件$optarg不存在"
+		exit 1
+	fi
+
+}
+
+msg(){
+	Msg="$optarg"
+}
+
+touser(){
+	UserID="$optarg"
+}
+
+toparty(){
+	PartyID="$optarg"
+}
+
+while getopts "f:m:u:p:" arg #选项后面的冒号表示该选项需要参数
+do
+	case $arg in
+		f)
+			load_config_file
+
+		m)
+			msg
+
+		u)
+			touser
+			
+		p)
+			toparty
+		?)  #当有不认识的选项的时候arg为?
+			echo "unkonw argument"
+			exit 1
+
+	esac
+done
