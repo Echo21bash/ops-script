04b8222cc4b012b88103a43413aa65e4f14e47b8
hebaodan <hebaodan21@qq.com>
2021-03-26 16:26:37 +0800

    添加POD监控项

--- zabbix/k8s/pod_status.sh
+++ zabbix/k8s/pod_status.sh
@@ -1,40 +1,63 @@
 #!/bin/bash
-# this kubernetes pod status
-# pod restart status
+#通过读取文件获取，降低k8s压力，防止zabbix执行超时
+#其他的监控项依赖于pod.status[all]监控项
+
+
 metric=$1
 pod_name=$2
 
-kubectl get deployments.apps | awk '{print$1}' | sed '1d' > /tmp/apps.txt
-kubectl top pod | sed '1d' > /tmp/top_status.txt
-kubectl get pod | sed '1d' > /tmp/pod_status.txt
+get_pod_all_status(){
 
+        kubectl get deployments.apps | awk '{print$1}' | sed '1d' > /tmp/apps.txt
+        kubectl top pod | sed '1d' > /tmp/top_status.txt
+        kubectl get pod | sed '1d' > /tmp/pod_status.txt
+        echo $?
+}
 
 get_pod_cpu_status(){
 
-	cat /tmp/top_status.txt | grep ${pod_name} | awk '{print$2}' | grep -oE "[0-9]{1,}"
+        cat /tmp/top_status.txt | grep ${pod_name} | awk '{print$2}' | grep -oE "[0-9]{1,}"
 
 }
 
 get_pod_mem_status(){
 
-	cat /tmp/top_status.txt | grep ${pod_name} | awk '{print$3}' | grep -oE "[0-9]{1,}"
+        cat /tmp/top_status.txt | grep ${pod_name} | awk '{print$3}' | grep -oE "[0-9]{1,}"
 }
 
 get_pod_status(){
 
-	cat /tmp/pod_status.txt | grep ${pod_name} | awk '{print$3}'
+        cat /tmp/pod_status.txt | grep ${pod_name} | awk '{print$3}'
 
 }
 
-case $metric in
-	cpu)
-		get_pod_cpu_status
-        ;;
-	mem)
-		get_pod_mem_status
-        ;;
-	stat)
-		get_pod_status
-        ;;
+get_pod_no_running(){
 
+        cat /tmp/pod_status.txt | awk '{print$3}' | grep -v "Running" | wc -l
+}
+
+get_pod_running(){
+
+        cat /tmp/pod_status.txt | awk '{print$3}' | grep "Running" | wc -l
+}
+
+case $metric in
+        cpu)
+                get_pod_cpu_status
+        ;;
+        mem)
+                get_pod_mem_status
+        ;;
+        stat)
+                get_pod_status
+        ;;
+        no_running)
+                get_pod_no_running
+        ;;
+        running)
+                get_pod_running
+        ;;
+        all)
+                get_pod_all_status
+        ;;
 esac
