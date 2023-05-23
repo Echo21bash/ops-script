c9947351b62bbddd42926ed4b627a2a3245cce12
hebaodanroot <rootww21@gmail.com>
2023-05-23 18:12:25 +0800

    更新

--- script/inotify-tools+rsync/docker-entrypoint.sh
+++ script/inotify-tools+rsync/docker-entrypoint.sh
@@ -35,18 +35,18 @@ EOF
 	chmod  600 /etc/rsyncd.secret
 	chmod  600 /etc/rsyncd.conf
 	[[ ! -d ${RSYNCD_PATH} ]] && mkdir -p ${RSYNCD_PATH}
-	rsync --no-detach --daemon --config /etc/rsyncd.conf
+	rsync --no-detach --daemon --config /etc/rsyncd.conf && "$@"
 elif [[ ${RUN_MODE} = 'sersync' ]];then
 	#内核参数修改
 	echo 'fs.inotify.max_user_watches = 999999' >> /etc/sysctl.conf
 	echo 'fs.inotify.max_user_instances = 1024' >> /etc/sysctl.conf
 	echo 'fs.inotify.max_queued_events = 999999' >> /etc/sysctl.conf
 	#内核参数生效
-	sudo sysctl -p
+	sysctl -p
 	#设置权限
 	echo "${RSYNCD_PASSWD}" >/etc/rsync.passwd
 	chmod 600 /etc/rsync.passwd
 	#启动进程
 	/usr/local/sersync/bin/inotify.sh -f /usr/local/sersync/etc/sersync.conf &
-	tail -F /usr/local/sersync/logs/rsync-err.log
+	tail -F /usr/local/sersync/logs/rsync-err.log && "$@"
 fi
